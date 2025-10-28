209803a3c71ec0db7c6ab54776e374e712155c26
big0010 <mmmjk087@gmail.com>
2025-10-28 17:52:21 +0700

    first commit

---
+++ lab2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int age=17;
+    char name{50}="saw chit To";
+    char gender = 'M';
+    float gpa=3.59;
+
+    printf("My name  %s", name);  //เเสดงข้อความ String
+    printf("I'm %d", age);  // เลขจำนวนเต็ม % Decime
+    printf("Grade %f \n", gpa); //float is decimal f 
+    printf("Gender%c \n", gender); // char is c
+    // printf("")
+    return 0;
+}
+
+////  %f float, %c =char
